5ffa153263ed219a5cbb30e4d46619b23b701e70
angelinalex <60354819+angelinalex@users.noreply.github.com>
2020-02-06 22:12:18 -0500

    Book Hw
    In Progress

--- Book.java
+++ Book.java
@@ -1,26 +1,113 @@
-/**
+ /**
  * A class that maintains information on a book.
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Angelina A Joy)
+ * @version (2/06/2020)
  */
-class Book
+ class Book
 {
      // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber= "";
+    private int borrowed; 
+    private boolean courseText;
+   
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, String refNumber, boolean courseText)
     {
         author = bookAuthor;
         title = bookTitle;
+        
+        courseText = false;
+        
+        
+        
     }
-
-    // Add the methods here ...
+    
+    // getPages method returns number of pages
+    // return pages field
+    
+    public int getPages()
+    { 
+        return pages;
+    }
+    
+    
+    //getTitle method returns title 
+    public String getTitle()
+    { 
+        return title;
+    }
+    
+    public String getAuthor()
+    {
+        return author;   
+    }
+    
+    public String getRefNumber()
+    {
+       return refNumber;
+    }
+     
+    
+    public void setRefNumber(String ref)
+    {
+       refNumber = ref; 
+       if(ref.length() >=3) 
+       refNumber = ref;
+            else 
+                System.out.println("Error input value!!!");
+     
+    }
+    
+    
+    public int getBorrowed()
+    {
+        return borrowed;
+    } 
+    
+    public void setBorrowed()
+    { 
+       borrowed+=1; 
+ 
+    }
+    
+    public void printAuthor()
+    {
+        System.out.println("Author:" + author);
+    }
+ 
+    public void printTitle()
+    {
+        System.out.println("Title:" + title);
+    }
+    
+    public void printDetails() 
+    {
+       System.out.print("Title: " + title);
+       System.out.print(" Author: " + author);
+       System.out.print(" Pages: " + pages);
+       
+       if(refNumber.length() == 0)
+           System.out.print(", Ref Number: ZZZ");
+           else 
+           System.out.print(", Ref Number: " + refNumber);
+           
+       System.out.print(" Times borrowed: " + borrowed);
+       
+    }
+    
+    public boolean isCourseText()
+    {
+        return courseText;
+    }
+    
 }
--- BookTest.java
+++ BookTest.java
@@ -27,96 +27,99 @@ import org.junit.Test;
  * Select the entire class and use the uncomment shortcut from the 
  * edit menu
  */
-// public class BookTest
-// {
-    // private Book book1;
-    // private Book book2;
-    // private Book book3;
+public class BookTest
+{
+    private Book book1;
+    private Book book2;
+    private Book book3;
 
 
-    // /**
-     // * Default constructor for test class BookTest
-     // */
-    // public BookTest()
-    // {
-    // }
+    /**
+     * Default constructor for test class BookTest
+     */
+    public BookTest()
+    {
+    }
 
-    // /**
-     // * Sets up the test fixture.
-     // *
-     // * Called before every test case method.
-     // */
-    // @Before
-    // public void setUp()
-    // {
-        // book1 = new Book("Barnes & Kolling", "Objects First with BlueJ 6th Ed", 666, true);
-        // book2 = new Book("Marting, George RRRRRRRR", "Game of Clones", 2042, false);
-        // book3 = new Book("Jackson, Steven", "Uncle Albert's Guide to Automotive Combat", 74, false);
-    // }
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+        book1 = new Book("Barnes & Kolling", "Objects First with BlueJ 6th Ed", 666, true);
+        book2 = new Book("Marting, George RRRRRRRR", "Game of Clones", 2042, false);
+        book3 = new Book("Jackson, Steven", "Uncle Albert's Guide to Automotive Combat", 74, false);
+    }
 
-    // /**
-     // * Tears down the test fixture.
-     // *
-     // * Called after every test case method.
-     // */
-    // @After
-    // public void tearDown()
-    // {
-    // }
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
 
-    // @Test
-    // public void getAuthor()
-    // {
-        // assertEquals("Marting, George RRRRRRRR", book2.getAuthor());
-        // assertEquals("Barnes & Kolling", book1.getAuthor());
-        // assertEquals("Jackson, Steven", book3.getAuthor());
-    // }
+    @Test
+    public void getAuthor()
+    {
+        assertEquals("Marting, George RRRRRRRR", book2.getAuthor());
+        assertEquals("Barnes & Kolling", book1.getAuthor());
+        assertEquals("Jackson, Steven", book3.getAuthor());
+    }
 
-    // @Test
-    // public void getTitle()
-    // {
-        // assertEquals("Objects First with BlueJ 6th Ed", book1.getTitle());
-        // assertEquals("Game of Clones", book2.getTitle());
-        // assertEquals("Uncle Albert's Guide to Automotive Combat", book3.getTitle());
-    // }
+    @Test
+    public void getTitle()
+    {
+        assertEquals("Objects First with BlueJ 6th Ed", book1.getTitle());
+        assertEquals("Game of Clones", book2.getTitle());
+        assertEquals("Uncle Albert's Guide to Automotive Combat", book3.getTitle());
+    }
 
-    // @Test
-    // public void isCourseText()
-    // {
-        // assertEquals(true, book1.isCourseText());
-        // assertEquals(false, book2.isCourseText());
-        // assertEquals(false, book3.isCourseText());
-    // }
+    @Test
+    public void isCourseText()
+    {
+        assertEquals(true, book1.isCourseText());
+        assertEquals(false, book2.isCourseText());
+        assertEquals(false, book3.isCourseText());
+    }
 
-    // @Test
-    // public void getPages()
-    // {
-        // assertEquals(666, book1.getPages());
-        // assertEquals(2042, book2.getPages());
-        // assertEquals(74, book3.getPages());
-    // }
+    @Test
+    public void getPages()
+    {
+        assertEquals(666, book1.getPages());
+        assertEquals(2042, book2.getPages());
+        assertEquals(74, book3.getPages());
+    }
 
-    // @Test
-    // public void borrowBook()
-    // {
-        // book1.borrow();
-        // assertEquals(1, book1.getBorrowed());
-        // book2.borrow();
-        // book2.borrow();
-        // book2.borrow();
-        // assertEquals(3, book2.getBorrowed());
-        // assertEquals(0, book3.getBorrowed());
-    // }
+    @Test
+    public void borrowBook()
+    {
+        book1.borrow();
+        assertEquals(1, book1.getBorrowed());
+        book2.borrow();
+        book2.borrow();
+        book2.borrow();
+        assertEquals(3, book2.getBorrowed());
+        assertEquals(0, book3.getBorrowed());
+    }
 
-    // @Test
-    // public void setReferenceNumber()
-    // {
-        // book1.setRefNumber("ez");
-        // assertEquals("", book1.getRefNumber());
-        // book1.setRefNumber("BaKo6ed");
-        // assertEquals("BaKo6ed", book1.getRefNumber());
-    // }
-// }
+    @Test
+    public void setReferenceNumber()
+    {
+        book1.setRefNumber("ez");
+        assertEquals("", book1.getRefNumber());
+        book1.setRefNumber("BaKo6ed");
+        assertEquals("BaKo6ed", book1.getRefNumber());
+    }
+}
+
+
+
 
 
 
